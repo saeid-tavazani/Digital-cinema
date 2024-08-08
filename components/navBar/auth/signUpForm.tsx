8da55662f7fd5df22905df8742c9d690a4763843
saeid-tavazani <saeidtavazani@gmail.com>
2024-08-08 11:37:33 +0330

    add forms auth
    --singup from
    --singin from

---
+++ components/navBar/auth/signUpForm.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { type FormEvent, type ReactNode } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+
+export default function SignUpForm() {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+
+  return (
+    <>
+      <form className="my-8 flex flex-col gap-3" onSubmit={handleSubmit}>
+        <LabelInputContainer>
+          <Label htmlFor="firstname">نام و نام خانوادگی</Label>
+          <Input id="firstname" placeholder="علی محمدی" type="text" />
+        </LabelInputContainer>
+        <LabelInputContainer>
+          <Label htmlFor="number">شماره موبایل</Label>
+          <Input id="number" placeholder="09130000000" type="text" />
+        </LabelInputContainer>
+        <LabelInputContainer>
+          <Label htmlFor="email">ایمیل</Label>
+          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer>
+          <Label htmlFor="password">رمزعبور</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+        <LabelInputContainer>
+          <Label htmlFor="repetpass">تکرار رمزعبور</Label>
+          <Input id="repetpass" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+
+        <button
+          className="bg-gradient-to-br mt-8 relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          ثبت نام
+          <BottomGradient />
+        </button>
+      </form>
+    </>
+  );
+}
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
