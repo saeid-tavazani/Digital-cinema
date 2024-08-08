75c8ec1eedfdc9abc81877b9d0de09f7067bca7a
saeid-tavazani <saeidtavazani@gmail.com>
2024-08-08 14:37:39 +0330

    fix navbar

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -1,5 +1,20 @@
+import AuthLayout from "./auth/authLayout";
+import LogoSite from "../ui/logoSite";
+import { NavNavHeader } from "./navLink";
+// import { NavHeader, NavMobile } from "./navLink";
+
 const index = () => {
-  return <div></div>;
+  return (
+    <header className="h-16 z-50 w-full fixed top-0 left-0 right-0">
+      <nav className="container flex items-center gap-4 justify-between h-full">
+        <div className="flex items-center gap-3">
+          <AuthLayout />
+          <NavNavHeader />
+        </div>
+        <LogoSite />
+      </nav>
+    </header>
+  );
 };
 
 export default index;
