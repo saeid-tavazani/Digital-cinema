358619d4d17465fe80491e40f041f5ed960ce239
saeid-tavazani <saeidtavazani@gmail.com>
2025-01-24 11:33:30 +0330

    Add remote image patterns to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'advance-movie-api.p.rapidapi.com',
+      },
+      { hostname: 'media.themoviedb.org' },
+      { hostname: 'img.cdno.my.id' },
+    ],
+  },
 };
 
 export default nextConfig;
